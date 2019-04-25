476dff64e459baae2367bb53867f06724089c291
dewei137 <k9282000@gmail.com>
2019-04-25 20:45:49 +0800

    20190425 from YZU

--- PLA.py
+++ PLA.py
@@ -4,11 +4,13 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import time
 import random
 import math
+from sklearn.metrics import accuracy_score
+
 
 class Perceptron(object):
     
     def __init__(self):
-        self.max_iteration = 5000
+        self.max_iteration = 7000
         self.targetLabel = 'Survived'
         self.pocketWeight = None
         self.pocketWeight_errNum = 0
@@ -69,32 +71,48 @@ class Perceptron(object):
 
     def predict(self,features):
         labels = []
-        for feature in features:
-            x = list(feature)
-            x.append(1)
-            labels.append(self.predict_(x))
+        for index , feature in features.iterrows():
+            feature = feature.append( pd.Series([1.0], ['x0']))
+            wx = 0
+            for fName , x in feature.iteritems():
+                w = self.weight[self.featureNameList.index(fName)]
+                if math.isnan(x) : 
+                    x = 0
+                wx = wx + (x * w)
+                
+            if wx<=0:
+                predict_sign = 0
+            else:
+                predict_sign = 1
+            labels.append(predict_sign)
         return labels
+            # break
+        #     x = list(feature)
+        #     x.append(1)
+        #     labels.append(self.predict_(x))
+        # return labels
 
     def predict_(self, x):
         wx = sum([self.weight[j] * x[j] for j in range(len(self.weight))])
         return int(wx > 0)
 
-if __name__ == '__main__': #模組名稱
+def preProcess(data):
+    
+    # Sex trans to sex_code 1/0
+    data['sex_code'] = data['Sex'].map({'female':1,'male':0}).astype('int')
 
-    #----------------EXAMPLE---------------
-    # raw_data = pd.read_csv('./data/example.csv', header=0)
-    # # print(raw_data)
-    # train_data = raw_data[['str']]
-    # target_data = raw_data[['isStr']]
-    # p = Perceptron()
-    # p.targetLabel = 'isStr'
-    # p.train(train_data, target_data)
-    #--------------------------------------
-    
-    
-    
-    # print ('Start read data')
-    # time_1 = time.time()
+    # get average of Age
+    age_mean = int(data['Age'].mean())
+
+    # replace nan to average in age
+    data['Age'] = data['Age'].transform(lambda x: age_mean if math.isnan(x) else x)
+
+    # filter feature
+    return  data[['sex_code','Pclass','Age','SibSp','Parch','Fare']]
+
+   
+
+if __name__ == '__main__': #模組名稱
 
     # get trainning data from csv file
     raw_data = pd.read_csv('./data/train.csv', header=0)
@@ -102,26 +120,22 @@ if __name__ == '__main__': #模組名稱
     # extract target value
     target_data = raw_data[['Survived']]
 
-    # Sex trans to sex_code 1/0
-    raw_data['sex_code'] = raw_data['Sex'].map({'female':1,'male':0}).astype('int')
-
-    # get average of Age
-    age_mean = int(raw_data['Age'].mean())
-
-    # replace nan to average in age
-    raw_data['Age'] = raw_data['Age'].transform(lambda x: age_mean if math.isnan(x) else x)
-
-    # filter feature
-    train_data = raw_data[['sex_code','Pclass','Age','SibSp','Parch','Fare']]
-   
-    # time_2 = time.time()
-    # print ('read data cost ', time_2 - time_1, ' second')
-        
-    # print ('Start training')
+    # data preprocee    
+    train_data = preProcess(raw_data)
+    
     p = Perceptron()
     p.train(train_data, target_data)
+    
+    test_data = pd.read_csv('./data/test.csv', header=0)
+    
 
-    # time_3 = time.time()
-    # print ('training cost ', time_3 - time_2, ' second', '\n')
+    # data preprocee    
+    test_data = preProcess(test_data)
+    test_predict = p.predict(test_data) 
 
-    test_predict = p.predict(test_features)
+    test_labels = pd.read_csv('./data/gender_submission.csv', header=0)
+    test_labels = [x[0] for x in test_labels[['Survived']].values.tolist()]
+    score = accuracy_score(test_labels, test_predict)
+
+    print(score)
+    
